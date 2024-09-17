840a0c8d7cec6e9fd7069d147a0b9d10dab6360c
Łukasz Królicki <lukasmatrix12345@gmail.com>
2024-09-17 12:32:19 +0200

    Main page implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import HomeCards from './components/HomeCards.jsx'
+import JobListings from './components/JobListings.jsx'
+import ViewAllJovs from './components/ViewAllJovs.jsx'
 const App = () => {
   return (
-    <div className='text-5xl'>
-      App
-    </div>
+    <>
+    <Navbar />
+    <Hero title="Test Title" subTitle="This is subtitle"/>
+    <HomeCards></HomeCards>
+    <JobListings></JobListings>
+    <ViewAllJovs></ViewAllJovs>
+    </>
   )
 }
 
